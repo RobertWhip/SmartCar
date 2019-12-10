33ff79207fd135d22ae9967d5f1d5c0a31e19a2d
RobertWhip <il.cint@mail.ru>
2019-12-10 18:56:25 +0200

    Update camera.py

--- Client/camera.py
+++ Client/camera.py
@@ -1,13 +1,21 @@
 import cv2
-
+import picamera
+import picamera.array
 
 class Vision:
-    def __init__(self):
-        self.__cap = cv2.VideoCapture(0)
+    def __init__(self, resolution=(640,480)):
+        self.__camera = picamera.PiCamera()
+        self.__stream = picamera.array.PiRGBArray(self.__camera)
+        self.__camera.resolution = resolution
+        self.__camera.color_effects = (128,128) # gray
 
-    def get_gray_image(self, width=640, height=480):
-        return cv2.resize(cv2.cvtColor(self.__cap.read()[1], cv2.COLOR_BGR2GRAY), (width, height))
+    def get_gray_image(self):
+        self.__camera.capture(self.__stream, 'bgr', use_video_port=True)
+        img = self.__stream.array
+        self.__stream.seek(0)
+        self.__stream.truncate()
+        return img
 
     def __del__(self):
-        self.__cap.release()
-
+        self.__camera.close()
+        self.__stream.close()
